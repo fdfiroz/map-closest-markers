c1021580f7f2799f53a8a67e646ec13d3cbd528c
firoz.ahammad <firoz.ahammad@spectrum-bd.com>
2024-06-06 10:22:12 +0600

    feat: Add lazy loading for PageHome component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import PageHome from '@/components/PageHome';
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('@/components/PageHome'),
+  { ssr: false }
+)
 export default function Home() {
   
   return (
     <>
-    <PageHome/>
+    <DynamicComponentWithNoSSR/>
     </>
     
   );
